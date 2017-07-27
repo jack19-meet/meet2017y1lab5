f2743dfe96d5696d925300e2898820da342938cd
jack19-meet <jack19@meet.mit.edu>
2017-07-27 13:58:00 +0300

    Stopping here

---
+++ fun1.py
@@ -0,0 +1,11 @@
+def add_number():
+    c=0
+    for number in range(1, 100  + 1):
+        print (number)
+        c = c+number
+    return c
+
+answer = add_number()
+print(answer)
+
+5
---
+++ h.py
@@ -0,0 +1,4 @@
+import turtle
+turtle.go to 1
+
+ 
